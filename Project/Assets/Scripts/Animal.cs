a5077ca7958ec54fd8d8ee4b0cb98365b0364112
Bryler C <bryler.castle@gmail.com>
2022-08-04 15:46:07 -0400

    Initial Commit

---
+++ Project/Assets/Scripts/Animal.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    enum State
+    {
+        searching,
+        getting
+    }
+
+    [SerializeField] string diet = "Plant";
+    [SerializeField] float aboveWater;
+    List<GameObject> foodSeen;
+    public float food = 100;
+    public Color color;
+    public float speed;
+    GameObject target;
+    SpriteRenderer sr;
+    float dir = .1f;
+    float primaryY;
+    State state;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        var r = Random.Range(1, 11);
+
+        switch (r)
+        {
+            case 1: color = Color.green; break;
+            case 2: color = Color.blue; break;
+            case 3: color = Color.red; break;
+            case 4: color = Color.black; break;
+            case 5: color = Color.grey; break;
+            case 6: color = Color.cyan; break;
+            case 7: color = Color.clear; break;
+            case 8: color = Color.gray; break;
+            case 9: color = Color.magenta; break;
+            case 10: color = Color.yellow; break;
+        }
+
+        sr.color = color;
+
+        speed = Random.Range(.01f, .2f);
+
+        primaryY = transform.position.y;
+
+        foodSeen = new List<GameObject>();
+
+
+        state = State.searching;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(diet))
+        {
+            food = 100;
+            state = State.searching;
+            target = null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(diet))
+        {
+            foodSeen.Add(collision.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        DoStates();
+        CheckNeeds();
+        SimulateNeeds();
+        TurnTowardDir();
+        CheckForBounds();
+    }
+
+    void TurnTowardDir()
+    {
+        if (dir > 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Camera.main.GetComponent<Cam>().ClickedOn(gameObject);
+    }
+
+    void CheckForBounds()
+    {
+        if (transform.position.x > 50)
+        {
+            dir = -speed;
+        }
+        else if (transform.position.x < -50)
+        {
+            dir = speed;
+        }
+    }
+
+    void SimulateNeeds()
+    {
+        food -= Time.deltaTime * 5;
+    }
+
+    void CheckNeeds()
+    {
+        if (food < 50)
+        {
+            if (foodSeen.Count == 0)
+            {
+                state = State.searching;
+            }
+            else
+            {
+                state = State.getting;
+            }
+        }
+    }
+
+    void DoStates()
+    {
+        if (state == State.searching)
+        {
+            if (transform.position.y != primaryY)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector3(transform.position.x, primaryY, 0), .1f);
+            }
+
+            transform.Translate(dir, 0, 0);
+        }
+        else if (state == State.getting)
+        {
+            if (foodSeen.Count == 0)
+            {
+                state = State.searching;
+            }
+            else if (target)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, .1f);
+            }
+            else if (foodSeen.Count > 0)
+            {
+                target = ClosestFood();
+            }
+        }
+    }
+
+    GameObject ClosestFood()
+    {
+        GameObject closest = null;
+
+        for (int i = 0; i < foodSeen.Count; i++)
+        {
+            if (!closest)
+            {
+                closest = foodSeen[i];
+            }
+            else if (Vector2.Distance(transform.position, foodSeen[i].transform.position) < Vector2.Distance(transform.position, closest.transform.position))
+            {
+                closest = foodSeen[i];
+            }
+        }
+
+        return closest;
+    }
+}
---
+++ Project/Assets/Scripts/Cam.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Cam : MonoBehaviour
+{
+    [SerializeField] Image popupMenu;
+    public GameObject target;
+    float scrollPos;
+
+    void Start()
+    {
+
+    }
+
+    public void ClickedOn(GameObject obj)
+    {
+        if (!target) { target = obj; popupMenu.gameObject.SetActive(true); }
+        else if(obj == target)
+        {
+            target = null;
+            popupMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            target = obj;
+            popupMenu.gameObject.SetActive(true);
+        }
+    }
+
+    void LateUpdate()
+    {
+        var wheelInput = Input.GetAxis("Mouse ScrollWheel");
+
+        if (wheelInput < 0 && scrollPos < 47)
+        {
+            scrollPos += 2;
+        }
+        else if(wheelInput > 0 && scrollPos > -47)
+        {
+            scrollPos -= 2;
+        }
+
+        if (target)
+        {
+            UpdateInfo(target.GetComponent<Animal>());
+            transform.position = Vector3.Lerp(transform.position, target.transform.position - new Vector3(0, 0, 10), Time.deltaTime * 2);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(scrollPos, 0, -10), Time.deltaTime * 2);
+        }
+    }
+
+    void UpdateInfo(Animal a)
+    {
+        popupMenu.transform.Find("HungerBar").GetComponent<Image>().fillAmount = a.food / 100;
+        popupMenu.transform.Find("SpeedHeader").Find("SpeedText").GetComponent<TMP_Text>().text = $"{(a.speed * 10).ToString("0.00")}";
+    }
+}
